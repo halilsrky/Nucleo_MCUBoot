fce53326ca63d5bc81801e7d9cdadb2bda5add70
halilsrky <halilsarikaya070@gmail.com>
2026-04-30 09:43:57 +0300

    Reinitialize repository: add .gitignore and project files

---
+++ src/main.c
@@ -0,0 +1,38 @@
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/sys/printk.h>
+
+#define LED0_NODE DT_ALIAS(led0)
+
+#if !DT_NODE_HAS_STATUS(LED0_NODE, okay)
+#error "led0 alias is not defined in devicetree"
+#endif
+
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+
+static int led_init(void)
+{
+	if (!gpio_is_ready_dt(&led)) {
+		printk("LED device is not ready\n");
+		return -1;
+	}
+
+	return gpio_pin_configure_dt(&led, GPIO_OUTPUT_INACTIVE);
+}
+
+int main(void)
+{
+	if (led_init() < 0) {
+		printk("LED init failed\n");
+	}
+
+	printk("Hello from app\n");
+
+	while (1) {
+		gpio_pin_toggle_dt(&led);
+		printk("App running...\n");
+		k_msleep(1000);
+	}
+
+	return 0;
+}
